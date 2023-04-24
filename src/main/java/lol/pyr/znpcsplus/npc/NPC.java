00651d9400583cf64d5b95d7dfd94ac260478943
Pyrbu <pyrmcserver@gmail.com>
2023-04-24 19:10:55 +0100

    stuff

--- src/main/java/lol/pyr/znpcsplus/npc/NPC.java
+++ src/main/java/lol/pyr/znpcsplus/npc/NPC.java
@@ -1,4 +1,100 @@
 package lol.pyr.znpcsplus.npc;
 
+import lol.pyr.znpcsplus.entity.PacketEntity;
+import lol.pyr.znpcsplus.entity.PacketLocation;
+import lol.pyr.znpcsplus.properties.NPCProperty;
+import lol.pyr.znpcsplus.properties.NPCPropertyKey;
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class NPC {
+    private final Set<Player> viewers = new HashSet<>();
+    private final String worldName;
+    private PacketEntity entity;
+    private PacketLocation location;
+    private NPCType type;
+
+    private final Map<NPCPropertyKey, NPCProperty> propertyMap = new HashMap<>();
+
+    public NPC(World world, NPCType type, PacketLocation location) {
+        this.worldName = world.getName();
+        this.type = type;
+        this.location = location;
+        entity = new PacketEntity(type.getType(), location); // TODO: Entity ID Provider
+    }
+
+    public void setType(NPCType type) {
+        _hideAll();
+        this.type = type;
+        entity = new PacketEntity(type.getType(), entity.getLocation());
+        _showAll();
+    }
+
+    public NPCType getType() {
+        return type;
+    }
+
+    public PacketLocation getLocation() {
+        return location;
+    }
+
+    public void setLocation(PacketLocation location) {
+        this.location = location;
+        entity.setLocation(location, viewers);
+    }
+
+    public World getWorld() {
+        return Bukkit.getWorld(worldName);
+    }
+
+    public void delete() {
+        _hideAll();
+        viewers.clear();
+    }
+
+    public void show(Player player) {
+        if (viewers.contains(player)) return;
+        _show(player);
+        viewers.add(player);
+    }
+
+    private void _show(Player player) {
+        entity.spawn(player);
+    }
+
+    public void hide(Player player) {
+        if (!viewers.contains(player)) return;
+        _hide(player);
+        viewers.remove(player);
+    }
+
+    private void _hide(Player player) {
+        entity.despawn(player);
+    }
+
+    private void _hideAll() {
+        for (Player viewer : viewers) _hide(viewer);
+    }
+
+    private void _showAll() {
+        for (Player viewer : viewers) _show(viewer);
+    }
+
+    public boolean isShown(Player player) {
+        return viewers.contains(player);
+    }
+
+    public NPCProperty getProperty(NPCPropertyKey key) {
+        return propertyMap.get(key);
+    }
+
+    public boolean hasProperty(NPCPropertyKey key) {
+        return propertyMap.containsKey(key);
+    }
 }
--- src/main/java/lol/pyr/znpcsplus/packets/PacketFactory.java
+++ src/main/java/lol/pyr/znpcsplus/packets/PacketFactory.java
@@ -2,20 +2,43 @@ package lol.pyr.znpcsplus.packets;
 
 import com.github.retrooper.packetevents.PacketEvents;
 import com.github.retrooper.packetevents.manager.server.ServerVersion;
+import lol.pyr.znpcsplus.entity.PacketEntity;
 import lol.pyr.znpcsplus.util.LazyLoader;
+import org.bukkit.entity.Player;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public interface PacketFactory {
+    void spawnPlayer(Player player, PacketEntity entity);
+    void spawnEntity(Player player, PacketEntity entity);
+    void destroyEntity(Player player, PacketEntity entity);
+    void teleportEntity(Player player, PacketEntity entity);
+    void addTabPlayer(Player player, PacketEntity entity);
+    void removeTabPlayer(Player player, PacketEntity entity);
+    void createTeam(Player player, PacketEntity entity);
+    void removeTeam(Player player, PacketEntity entity);
 
-    Map<ServerVersion, LazyLoader<? extends PacketFactory>> factories = buildFactoryMap();
-
+    PacketFactory factory = get();
 
     static PacketFactory get() {
-        return factories.get(PacketEvents.getAPI().getServerManager().getVersion()).get();
+        if (factory != null) return factory;
+        ServerVersion version = PacketEvents.getAPI().getServerManager().getVersion();
+        Map<ServerVersion, LazyLoader<? extends PacketFactory>> factories = buildFactoryMap();
+        if (factories.containsKey(version)) return factories.get(version).get();
+        for (ServerVersion v : ServerVersion.reversedValues()) {
+            if (v.isNewerThan(version)) continue;
+            if (!factories.containsKey(v)) continue;
+            return factories.get(v).get();
+        }
+        throw new RuntimeException("Unsupported version!");
     }
 
     private static Map<ServerVersion, LazyLoader<? extends PacketFactory>> buildFactoryMap() {
-
+        HashMap<ServerVersion, LazyLoader<? extends PacketFactory>> map = new HashMap<>();
+        map.put(ServerVersion.V_1_8, LazyLoader.of(V1_8Factory::new));
+        map.put(ServerVersion.V_1_14, LazyLoader.of(V1_14Factory::new));
+        map.put(ServerVersion.V_1_19, LazyLoader.of(V1_19Factory::new));
+        return map;
     }
 }
