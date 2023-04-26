72134f8d740708ab4ad153ad3be432057dfa4a33
Pyrbu <pyrmcserver@gmail.com>
2023-04-26 17:08:14 +0100

    use this amazing config library i just found

---
+++ src/main/java/lol/pyr/znpcsplus/config/MessageConfig.java
@@ -0,0 +1,38 @@
+package lol.pyr.znpcsplus.config;
+
+/**
+ * (OLD CONFIGURATION)
+ *     NO_PERMISSION("messages", "&cYou do not have permission to execute this command.", String.class),
+ *     SUCCESS("messages", "&aSuccess!", String.class),
+ *     INCORRECT_USAGE("messages", "&cThe arguments you specified are invalid. Type &f/znpcs&c for examples.", String.class),
+ *     COMMAND_NOT_FOUND("messages", "&cThe command you specified does not exist!", String.class),
+ *     COMMAND_ERROR("messages", "&cAn error occurred when executing this command. See console for more information.", String.class),
+ *     INVALID_NUMBER("messages", "&cThe ID you have specified is invalid. Please use positive integers only!", String.class),
+ *     NPC_NOT_FOUND("messages", "&cNo NPCs could be found with this ID!", String.class),
+ *     TOO_FEW_ARGUMENTS("messages", "&cThis command does not contain enough arguments. Type &f/znpcs&c or view our documentation for a list/examples of existing arguments.", String.class),
+ *     PATH_START("messages", "&aSuccess! Move to create a path for your NPC. When finished, type &f/znpcs path exit&c to exit path creation.", String.class),
+ *     EXIT_PATH("messages", "&cYou have exited path creation.", String.class),
+ *     PATH_FOUND("messages", "&cThere is already a path with this name.", String.class),
+ *     NPC_FOUND("messages", "&cThere is already an NPC with this ID.", String.class),
+ *     NO_PATH_FOUND("messages", "&cThe path you have specified does not exist.", String.class),
+ *     NO_SKIN_FOUND("messages", "&cThe skin username/URL you have specified does not exist or is invalid.", String.class),
+ *     NO_NPC_FOUND("messages", "&cThe NPC you have specified does not exist.", String.class),
+ *     NO_ACTION_FOUND("messages", "&cThis action does not exist! Type &f/znpcs&c or view our documentation for a list/examples of existing action types.", String.class),
+ *     METHOD_NOT_FOUND("messages", "&cThis method does not exist! Type &f/znpcs&c or view our documentation for a list/examples of existing methods.", String.class),
+ *     INVALID_NAME_LENGTH("messages", "&cThe name you specified either too short or long. Please enter a positive integer of (3 to 16) characters.", String.class),
+ *     UNSUPPORTED_ENTITY("messages", "&cThis entity type not available in your current server version.", String.class),
+ *     PATH_SET_INCORRECT_USAGE("messages", "&eUsage: &aset <npc_id> <path_name>", String.class),
+ *     ACTION_ADD_INCORRECT_USAGE("messages", "&eUsage: &a<SERVER/CMD/MESSAGE/CONSOLE> <actionValue>", String.class),
+ *     ACTION_DELAY_INCORRECT_USAGE("messages", "&eUsage: &a<action_id> <delay>", String.class),
+ *     CONVERSATION_SET_INCORRECT_USAGE("messages", "&cUsage: <npc_id> <conversation_name> <RADIUS/CLICK>", String.class),
+ *     NO_CONVERSATION_FOUND("messages", "&cThe conversation you have specified does not exist!", String.class),
+ *     CONVERSATION_FOUND("messages", "&cThere is already a conversation with this name.", String.class),
+ *     INVALID_SIZE("messages", "&cThe position you have specified cannot exceed the limit.", String.class),
+ *     FETCHING_SKIN("messages", "&aFetching skin for name: &f%s&a. Please wait...", String.class),
+ *     CANT_GET_SKIN("messages", "&cCould not fetch skin for name: %s.", String.class),
+ *     GET_SKIN("messages", "&aSkin successfully fetched!", String.class),
+ *     NOT_SUPPORTED_NPC_TYPE("messages", "&cThis NPC type doesn't exists or is not supported in your current server version.", String.class);
+ */
+
+public interface MessageConfig {
+}
