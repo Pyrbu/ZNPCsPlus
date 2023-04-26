63c72fe04bc3daba74fda093465fd39211cbc69e
Pyrbu <pyrmcserver@gmail.com>
2023-04-26 17:24:47 +0100

    add Component serializer

--- src/main/java/lol/pyr/znpcsplus/config/MainConfig.java
+++ src/main/java/lol/pyr/znpcsplus/config/MainConfig.java
@@ -1,24 +1,28 @@
 package lol.pyr.znpcsplus.config;
 
+import space.arim.dazzleconf.annote.ConfComments;
 import space.arim.dazzleconf.annote.ConfKey;
 
 import static space.arim.dazzleconf.annote.ConfDefault.*;
 
-// TODO: Add comments to the values using @ConfComments()
 public interface MainConfig {
     @ConfKey("view-distance")
+    @ConfComments("How far away do you need to be from any NPC for it to disappear, measured in blocks")
     @DefaultInteger(32)
     int viewDistance();
 
     @ConfKey("line-spacing")
+    @ConfComments("The height between hologram lines, measured in blocks")
     @DefaultDouble(0.3D)
     double lineSpacing();
 
-    @ConfKey("debug-enabled")
-    @DefaultBoolean(false)
-    boolean debugEnabled();
-
     @ConfKey("check-for-updates")
+    @ConfComments("Should the plugin check for available updates and notify admins about them?")
     @DefaultBoolean(true)
     boolean checkForUpdates();
+
+    @ConfKey("debug-enabled")
+    @ConfComments({"Should debug mode be enabled?", "This is used in development to test various things, you probably don't want to enable this"})
+    @DefaultBoolean(false)
+    boolean debugEnabled();
 }
