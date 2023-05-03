9701b64e8127b2bbaab0b2fc54c65c7ee7ee2427
Pyrbu <pyrmcserver@gmail.com>
2023-05-03 05:16:33 +0100

    remove unused exception

--- spigot/src/main/java/io/github/znetworkw/znpcservers/UnexpectedCallException.java
+++
@@ -1,7 +0,0 @@
-package io.github.znetworkw.znpcservers;
-
-public class UnexpectedCallException extends RuntimeException {
-    public UnexpectedCallException(Throwable cause) {
-        super(cause);
-    }
-}
