74af9522aee5c47cfeff71abebddadc034fb157a
Pyrbu <pyrmcserver@gmail.com>
2023-05-11 07:14:39 +0100

    make property suggestions work

--- plugin/src/main/java/lol/pyr/znpcsplus/commands/PropertiesCommand.java
+++ plugin/src/main/java/lol/pyr/znpcsplus/commands/PropertiesCommand.java
@@ -8,6 +8,7 @@ import lol.pyr.znpcsplus.npc.NpcEntryImpl;
 import lol.pyr.znpcsplus.npc.NpcImpl;
 import lol.pyr.znpcsplus.npc.NpcRegistryImpl;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 
 import java.util.Collections;
 import java.util.List;
@@ -24,14 +25,14 @@ public class PropertiesCommand implements CommandHandler {
         // TODO: implement all the parsers for the types used in EntityPropertyImpl
         Object value = context.parse(property.getType());
         npc.UNSAFE_setProperty(property, value);
-        context.send(Component.text("Set property " + property.getName() + " for NPC " + entry.getId() + " to " + value.toString()));
+        context.send(Component.text("Set property " + property.getName() + " for NPC " + entry.getId() + " to " + value.toString(), NamedTextColor.GREEN));
     }
 
     @Override
     public List<String> suggest(CommandContext context) throws CommandExecutionException {
         if (context.argSize() == 1) return context.suggestCollection(NpcRegistryImpl.get().modifiableIds());
-        if (context.argSize() == 2) return context.suggestStream(context.suggestionParse(1, NpcEntryImpl.class)
-                .getNpc().getType().getAllowedProperties().stream().map(EntityPropertyImpl::getName));
+        if (context.argSize() == 2) return context.suggestStream(context.suggestionParse(0, NpcEntryImpl.class)
+                    .getNpc().getType().getAllowedProperties().stream().map(EntityPropertyImpl::getName));
         return Collections.emptyList();
     }
 }
