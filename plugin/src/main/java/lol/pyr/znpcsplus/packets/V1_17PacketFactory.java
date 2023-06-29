231ceba803eb55580fa4ee9a20406822bcad2d93
D3v1s0m <49519439+D3v1s0m@users.noreply.github.com>
2023-06-29 16:59:22 +0530

    Fixed non-player npc spawning in 1.14-1.16

--- plugin/src/main/java/lol/pyr/znpcsplus/packets/V1_17PacketFactory.java
+++ plugin/src/main/java/lol/pyr/znpcsplus/packets/V1_17PacketFactory.java
@@ -2,20 +2,33 @@ package lol.pyr.znpcsplus.packets;
 
 import com.github.retrooper.packetevents.PacketEventsAPI;
 import com.github.retrooper.packetevents.protocol.entity.data.EntityData;
+import com.github.retrooper.packetevents.util.Vector3d;
+import com.github.retrooper.packetevents.wrapper.play.server.WrapperPlayServerSpawnEntity;
 import lol.pyr.znpcsplus.api.entity.PropertyHolder;
 import lol.pyr.znpcsplus.entity.EntityPropertyRegistryImpl;
 import lol.pyr.znpcsplus.entity.PacketEntity;
 import lol.pyr.znpcsplus.metadata.MetadataFactory;
 import lol.pyr.znpcsplus.scheduling.TaskScheduler;
+import lol.pyr.znpcsplus.util.NpcLocation;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
 import java.util.Map;
+import java.util.Optional;
 
 public class V1_17PacketFactory extends V1_16PacketFactory {
     public V1_17PacketFactory(TaskScheduler scheduler, MetadataFactory metadataFactory, PacketEventsAPI<Plugin> packetEvents, EntityPropertyRegistryImpl propertyRegistry, LegacyComponentSerializer textSerializer) {
         super(scheduler, metadataFactory, packetEvents, propertyRegistry, textSerializer);
+    }
+
+    @Override
+    public void spawnEntity(Player player, PacketEntity entity, PropertyHolder properties) {
+        NpcLocation location = entity.getLocation();
+        sendPacket(player, new WrapperPlayServerSpawnEntity(entity.getEntityId(), Optional.of(entity.getUuid()), entity.getType(),
+                npcLocationToVector(location), location.getPitch(), location.getYaw(), location.getYaw(), 0, Optional.of(new Vector3d())));
+        sendAllMetadata(player, entity, properties);
+        createTeam(player, entity, properties);
     }
 
     @Override
