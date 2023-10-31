ca1416e761b953126073c846055bacc9eba2d120
Pyrbu <pyrmcserver@gmail.com>
2023-10-31 18:58:42 +0100

    revert this (why??)

--- plugin/src/main/java/lol/pyr/znpcsplus/hologram/HologramImpl.java
+++ plugin/src/main/java/lol/pyr/znpcsplus/hologram/HologramImpl.java
@@ -45,7 +45,7 @@ public class HologramImpl extends Viewable implements Hologram {
     }
 
     public void addTextLine(String line) {
-        addTextLineComponent(Component.text(line));
+        addTextLineComponent(textSerializer.deserialize(textSerializer.serialize(MiniMessage.miniMessage().deserialize(line))));
     }
 
     public void addItemLineStack(org.bukkit.inventory.ItemStack item) {
